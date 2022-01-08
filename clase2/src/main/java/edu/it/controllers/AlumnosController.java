9c79c0aae84121029f2d81c664b873fd4113a507
andy <arteysoft@gmail.com>
2022-01-08 12:09:28 -0300

    probar inventar y luego insertar

--- clase2/src/main/java/edu/it/controllers/AlumnosController.java
+++ clase2/src/main/java/edu/it/controllers/AlumnosController.java
@@ -1,16 +1,19 @@
 package edu.it.controllers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import javax.servlet.ServletException;
 
 public class AlumnosController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
-            response.setContentType("text/plain");
+    	response.setContentType("application/json");
             PrintWriter out = response.getWriter();
             out.println("<h1>Hora Actual: ");
             out.println(System.currentTimeMillis());
@@ -19,11 +22,22 @@ public class AlumnosController extends HttpServlet {
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-                response.setContentType("text/plain");
-                PrintWriter out = response.getWriter();
-                out.println("<h1>Agregando recurso: ");
-                out.println(System.currentTimeMillis());
-                out.println("</h1>");
+    			response.setContentType("application/json");
+                
+    			// Lectura del contenido entrante
+    			InputStreamReader isr = new InputStreamReader(request.getInputStream());
+    			BufferedReader br = new BufferedReader(isr);
+    			StringBuilder sb = new StringBuilder();
+    			for (String s = br.readLine(); s != null; s = br.readLine()) {
+    				sb.append(s);
+    			}
+    			// Fin lectura contenido entrante
+    			
+    			System.out.println(sb.toString());
+    			
+    			PrintWriter out = response.getWriter();
+                out.println("Alumno agregado de forma exitosa... ");
+                
                 response.setStatus(201);
         }
 }
