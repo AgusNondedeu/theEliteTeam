cad1e44deef7db93248daea457c0e3b8a9d08022
andy <arteysoft@gmail.com>
2022-01-08 12:46:38 -0300

    para practicar en la semana

--- clase2/src/main/java/edu/it/components/Utiles.java
+++ clase2/src/main/java/edu/it/components/Utiles.java
@@ -1,5 +1,8 @@
 package edu.it.components;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.UUID;
 
 import com.github.javafaker.Faker;
@@ -19,5 +22,22 @@ public class Utiles {
 	            fkr.address().state(),
 	            fkr.address().country()
 	        );
+	}
+	private static String leerInputStreamReaderManejado(InputStream inputStream) throws Exception {
+		InputStreamReader isr = new InputStreamReader(inputStream);
+		BufferedReader br = new BufferedReader(isr);
+		StringBuilder sb = new StringBuilder();
+		for (String s = br.readLine(); s != null; s = br.readLine()) {
+			sb.append(s);
+		}
+		return sb.toString();
+	}
+	public static String leerInputStreamReader(InputStream inputStream) {
+		try {
+			return leerInputStreamReaderManejado(inputStream);
+		}
+		catch (Exception ex) {
+			throw new RuntimeException(ex.getMessage());
+		}
 	}
 }
--- clase2/src/main/java/edu/it/controllers/AlumnosController.java
+++ clase2/src/main/java/edu/it/controllers/AlumnosController.java
@@ -1,14 +1,18 @@
 package edu.it.controllers;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import edu.it.components.ConectorJPA;
+import edu.it.components.Utiles;
+import edu.it.model.Alumno;
 
 public class AlumnosController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -25,15 +29,23 @@ public class AlumnosController extends HttpServlet {
     			response.setContentType("application/json");
                 
     			// Lectura del contenido entrante
-    			InputStreamReader isr = new InputStreamReader(request.getInputStream());
-    			BufferedReader br = new BufferedReader(isr);
-    			StringBuilder sb = new StringBuilder();
-    			for (String s = br.readLine(); s != null; s = br.readLine()) {
-    				sb.append(s);
-    			}
+    			String s = Utiles.leerInputStreamReader(request.getInputStream());
     			// Fin lectura contenido entrante
     			
-    			System.out.println(sb.toString());
+    			Gson gson = new Gson();
+    			Alumno a = gson.fromJson(s, Alumno.class);
+    			
+    			System.out.println(a.id);
+    			System.out.println(a.nombre);
+    			System.out.println(a.apellido);
+    			System.out.println(a.pais);
+    			
+    			var conn = new ConectorJPA();
+    		    var entityManager =	conn.getEntityManager();
+    		    var tx = entityManager.getTransaction();
+    		    tx.begin();
+    		    entityManager.merge(a);
+    		    tx.commit();
     			
     			PrintWriter out = response.getWriter();
                 out.println("Alumno agregado de forma exitosa... ");
