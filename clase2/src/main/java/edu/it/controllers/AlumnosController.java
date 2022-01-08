860669ac30c6e1a2a395a22da67432a0acfa5d54
andy <arteysoft@gmail.com>
2022-01-08 10:13:35 -0300

    ejercicio doPost

--- clase2/src/main/java/edu/it/controllers/AlumnosController.java
+++ clase2/src/main/java/edu/it/controllers/AlumnosController.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletException;
 public class AlumnosController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
-            response.setContentType("text/html");
+            response.setContentType("text/plain");
             PrintWriter out = response.getWriter();
             out.println("<h1>Hora Actual: ");
             out.println(System.currentTimeMillis());
